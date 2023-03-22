69c1f83efbb24bd8cf1036e2b1f98091dfaeaf54
klgibsonjr <klgibsonjr@gmail.com>
2023-03-21 22:07:55 -0700

    added a header and a footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer class=' bg-white rounded-lg shadow m-4 dark:bg-gray-800'>
+      <div class='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-between'>
+        <span class='text-lg text-gray-700 sm:text-center dark:text-gray-400'>
+          Ken Gibson{'  '}
+          <a href='https://flowbite.com/' class='hover:underline'></a>
+          Full-stack developer
+        </span>
+        <ul class='flex flex-wrap items-center gap-4 mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0'>
+          <li>
+            <a href='#' class='mr-4 hover:underline md:mr-6 '>
+              <BsLinkedin size={25} />
+            </a>
+          </li>
+          <li>
+            <a href='#' class='mr-4 hover:underline md:mr-6'>
+              <BsGithub size={25} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
