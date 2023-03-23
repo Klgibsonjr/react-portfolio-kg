711e8869ef9dcd174a4a60d355b0f9d923cb4028
klgibsonjr <klgibsonjr@gmail.com>
2023-03-23 10:35:34 -0700

    added functionality to mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
+  const [navToggle, setNavToggle] = useState(false);
+  const handleNavToggle = () => setNavToggle(!navToggle);
   return (
     <nav className='bg-slate-500 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900'>
       <div className='container flex flex-wrap items-center justify-between mx-auto'>
@@ -9,28 +12,7 @@ const Navbar = () => {
             Ken Gibson
           </span>
         </a>
-        {/* <button
-          data-collapse-toggle='navbar-default'
-          type='button'
-          className='inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
-          aria-controls='navbar-default'
-          aria-expanded='false'
-        >
-          <span className='sr-only'>Open main menu</span>
-          <svg
-            className='w-6 h-6'
-            aria-hidden='true'
-            fill='currentColor'
-            viewBox='0 0 20 20'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              fillRule='evenodd'
-              d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-              clipRule='evenodd'
-            ></path>
-          </svg>
-        </button> */}
+
         <div className='hidden w-full md:block md:w-auto' id='navbar-default'>
           <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-slate-500 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
             <li>
@@ -68,6 +50,45 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+
+        {/* Hamburger */}
+        <div
+          onClick={handleNavToggle}
+          className='cursor-pointer md:hidden z-10'
+        >
+          {!navToggle ? <FaBars /> : <FaTimes />}
+        </div>
+
+        {/* Mobile Menu */}
+        <ul
+          className={
+            !navToggle
+              ? 'hidden'
+              : 'absolute top-0 left-0 w-full h-screen bg-slate-600 flex flex-col justify-center items-center'
+          }
+        >
+          <li className='py-6 text-4xl'>
+            <a href='/pages/AboutMe' className=''>
+              About Me
+            </a>
+          </li>
+          <li className='py-6 text-4xl'>
+            <a href='/pages/Portfolio' className=''>
+              Portfolio
+            </a>
+          </li>
+
+          <li className='py-6 text-4xl'>
+            <a href='/pages/Resume' className=''>
+              Resume
+            </a>
+          </li>
+          <li className='py-6 text-4xl'>
+            <a href='/pages/ContactMe' className=''>
+              Contact Me
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
