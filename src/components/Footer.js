6e694f37022774556adba8f45a7c8451812ae642
klgibsonjr <klgibsonjr@gmail.com>
2023-03-23 11:14:34 -0700

    added functionality to contactMe form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,22 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs';
 
 const Footer = () => {
   return (
-    <footer class=' bg-slate-500 rounded-lg shadow fixed w-full bottom-0 dark:bg-gray-800'>
-      <div class='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-around'>
-        <a href='mailto:' class='text-xl  sm:text-center dark:text-gray-400'>
+    <footer className=' bg-slate-500 rounded-lg shadow fixed w-full bottom-0 dark:bg-gray-800'>
+      <div className='w-full mx-auto container md:p-6 p-4 md:flex md:items-center md:justify-around'>
+        <a
+          href='mailto:'
+          className='text-xl  sm:text-center dark:text-gray-400'
+        >
           Klgibsonjr@gmail.com
         </a>
-        <ul class='flex flex-wrap items-center gap-4 mt-3 text-sm  dark:text-gray-400 sm:mt-0'>
+        <ul className='flex flex-wrap items-center gap-4 mt-3 text-sm  dark:text-gray-400 sm:mt-0'>
           <li>
-            <a href='#' class='mr-4  md:mr-6 '>
+            <a href='#' className='mr-4  md:mr-6 '>
               <BsLinkedin size={30} />
             </a>
           </li>
           <li>
-            <a href='#' class='mr-4  md:mr-6'>
+            <a href='#' className='mr-4  md:mr-6'>
               <BsGithub size={30} />
             </a>
           </li>
--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,58 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ContactMe = () => {
+  const [nameInput, setNameInput] = useState('');
+  const [emailInput, setEmailInput] = useState('');
+  const [messageInput, setMessageInput] = useState('');
+
+  const handlInputChange = (e) => {
+    const { name, value } = e.target;
+
+    return name === 'name'
+      ? setNameInput(value)
+      : 'name'
+      ? setEmailInput(value)
+      : setMessageInput(value);
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    setNameInput('');
+    setEmailInput('');
+    setMessageInput('');
+  };
+
   return (
     <form className='w-[50vh] md:w-[100vh] mx-auto mt-8'>
-      <div class='mb-6'>
+      <div className='mb-6'>
         <label
-          for='email'
-          class='block m-2 text-lg font-medium text-gray-900 dark:text-white'
+          htmlFor='name'
+          className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
         >
           Name
         </label>
         <input
           type='text'
-          id='name'
-          class='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          name='name'
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           placeholder='Enter name here'
           required
         />
       </div>
-      <div class='mb-6'>
+      <div className='mb-6'>
         <label
-          for='password'
-          class='block m-2 text-lg font-medium text-gray-900 dark:text-white'
+          htmlFor='email'
+          className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
         >
           Email
         </label>
         <input
           type='email'
-          id='email'
-          class='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          name='email'
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           placeholder='Enter email here'
           required
         />
       </div>
-      <div class='flex items-start mb-6'>
-        <div class='flex items-center h-5'></div>
+      <div className='flex items-start mb-6'>
+        <div className='flex items-center h-5'></div>
       </div>
 
       <label
-        for='message'
-        class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+        htmlFor='message'
+        className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
       >
         Your message
       </label>
       <textarea
-        id='message'
+        name='message'
         rows='4'
-        class='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         placeholder='Write your thoughts here...'
       ></textarea>
 
       <button
         type='submit'
-        class='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        className='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
       >
         Submit
       </button>
