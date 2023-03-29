26017325f38f150df5c540704cfb1019dbb1904b
klgibsonjr <klgibsonjr@gmail.com>
2023-03-28 21:43:12 -0700

    working on form validation

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -20,11 +20,6 @@ const ContactMe = () => {
   const handleFormSubmit = (e) => {
     e.preventDefault();
 
-    if (!validateEmail(emailInput)) {
-      setErrorMessage('Please enter a valid input');
-      return;
-    }
-
     setNameInput('');
     setEmailInput('');
     setMessageInput('');
@@ -78,11 +73,13 @@ const ContactMe = () => {
       </label>
       <textarea
         value={messageInput}
+        type='text'
         name='message'
         onChange={handlInputChange}
         rows='4'
         className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         placeholder='Write your thoughts here...'
+        required
       ></textarea>
       <button
         type='submit'
