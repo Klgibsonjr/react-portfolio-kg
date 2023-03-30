c93fccbd0aed8dd53477ac12c54b998a8a0637a3
klgibsonjr <klgibsonjr@gmail.com>
2023-03-29 18:53:27 -0700

    added onBlur functionality to contact me page

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -60,6 +60,7 @@ const ContactMe = () => {
             placeholder='Enter name here'
             value={formValues.name}
             onChange={handleInputChange}
+            onBlur={handleFormSubmit}
             className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           />
         </div>
@@ -78,6 +79,7 @@ const ContactMe = () => {
             placeholder='Enter email here'
             value={formValues.email}
             onChange={handleInputChange}
+            onBlur={handleFormSubmit}
             className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
           />
         </div>
@@ -98,6 +100,7 @@ const ContactMe = () => {
           placeholder='Write your thoughts here...'
           value={formValues.message}
           onChange={handleInputChange}
+          onBlur={handleFormSubmit}
           className='block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
         ></textarea>
         <p className='text-md text-red-700'>{formErrors.message}</p>
