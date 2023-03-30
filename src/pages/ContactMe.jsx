ba7b49277f743dcc4e9218cc73b1fb1062fd3fc3
klgibsonjr <klgibsonjr@gmail.com>
2023-03-29 17:26:22 -0700

    completed form validation on contact page

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,94 +1,115 @@
-import React, { useState } from 'react';
-import { validateEmail } from '../utils/helpers';
+import React, { useState, useEffect } from 'react';
 
 const ContactMe = () => {
-  const [nameInput, setNameInput] = useState('');
-  const [emailInput, setEmailInput] = useState('');
-  const [messageInput, setMessageInput] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const formInputValues = { name: '', email: '', message: '' };
+  const [formValues, setFormValues] = useState(formInputValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [formSubmit, setFormSubmit] = useState(false);
 
-  const handlInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-
-    name === 'name'
-      ? setNameInput(value)
-      : name === 'email'
-      ? setEmailInput(value)
-      : setMessageInput(value);
+    setFormValues({ ...formValues, [name]: value });
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setFormErrors(validate(formValues));
+    setFormSubmit(true);
+  };
 
-    setNameInput('');
-    setEmailInput('');
-    setMessageInput('');
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && formSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {};
+    const regEx = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.name) {
+      errors.name = 'Name is required!';
+    }
+    if (!values.email) {
+      errors.email = 'Email is required!';
+    } else if (!regEx.test(values.email)) {
+      errors.email = 'This is not a valid email!';
+    }
+    if (!values.message) {
+      errors.message = 'A message is required!';
+    }
+    return errors;
   };
 
   return (
-    <form className='w-auto my-10 mx-10 md:w-[75vh] md:mx-auto '>
-      <div className='mb-6'>
-        <label
-          htmlFor='name'
-          className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
-        >
-          Name
-        </label>
-        <input
-          value={nameInput}
-          type='text'
-          name='name'
-          onChange={handlInputChange}
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-          placeholder='Enter name here'
-          required
-        />
-      </div>
+    <div>
+      <form
+        onSubmit={handleFormSubmit}
+        className='w-auto my-10 mx-10 md:w-[75vh] md:mx-auto '
+      >
+        <div className='mb-6'>
+          <label
+            // htmlFor='name'
+            className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
+          >
+            Name
+          </label>
+          <input
+            type='text'
+            name='name'
+            placeholder='Enter name here'
+            value={formValues.name}
+            onChange={handleInputChange}
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          />
+        </div>
+        <p className='text-md text-red-700'>{formErrors.name}</p>
 
-      <div className='mb-6'>
+        <div className='mb-6'>
+          <label
+            htmlFor='email'
+            className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
+          >
+            Email
+          </label>
+          <input
+            type='email'
+            name='email'
+            placeholder='Enter email here'
+            value={formValues.email}
+            onChange={handleInputChange}
+            className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          />
+        </div>
+        <p className='text-md text-red-700'>{formErrors.email}</p>
+        <div className='flex items-start mb-6'>
+          <div className='flex items-center h-5'></div>
+        </div>
         <label
-          htmlFor='email'
-          className='block m-2 text-lg font-medium text-gray-900 dark:text-white'
+          htmlFor='message'
+          className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
         >
-          Email
+          Your message
         </label>
-        <input
-          value={emailInput}
-          type='email'
-          name='email'
-          onChange={handlInputChange}
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-          placeholder='Enter email here'
-          required
-        />
-      </div>
-      <div className='flex items-start mb-6'>
-        <div className='flex items-center h-5'></div>
-      </div>
-      <label
-        htmlFor='message'
-        className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-      >
-        Your message
-      </label>
-      <textarea
-        value={messageInput}
-        type='text'
-        name='message'
-        onChange={handlInputChange}
-        rows='4'
-        className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        placeholder='Write your thoughts here...'
-        required
-      ></textarea>
-      <button
-        type='submit'
-        onClick={handleFormSubmit}
-        className='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-      >
-        Submit
-      </button>
-    </form>
+        <textarea
+          type='text'
+          name='message'
+          rows='4'
+          placeholder='Write your thoughts here...'
+          value={formValues.message}
+          onChange={handleInputChange}
+          className='block p-2.5 mb-4 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+        ></textarea>
+        <p className='text-md text-red-700'>{formErrors.message}</p>
+        <button
+          type='submit'
+          onClick={handleFormSubmit}
+          className='text-white mt-8 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+        >
+          Submit
+        </button>
+      </form>
+    </div>
   );
 };
 
